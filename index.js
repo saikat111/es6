525d9b6bbc2ce8b7d6d725ea9315ee8208a98672
saikat111 <s8856010@gmail.com>
2022-12-02 02:40:49 +0600

    task3

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 //task 1
 console.log('saikat', "rakib")
+
+
+
+//task 2
+
+var dogo = "go"
+console.log(dogo)
+var dogo = "you"
+console.log(dogo)
+//fucntion
+function printDogo(){
+    var dogo = "heheh"
+    console.log(dogo)
+}
+console.log(dogo)
+printDogo()
+
+let name = "saikat"//we can decler the name again
+
+for(var i =0; i<10; i++){
+    console.log(i)
+}
+
